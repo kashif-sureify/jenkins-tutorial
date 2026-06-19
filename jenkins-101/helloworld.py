b06cc512929c7b1aea8f1d26e610994babffc77d
kashif-sureify <mdkashifraza.ansari@sureify.com>
2026-06-19 19:35:19 +0530

    add additional greeting message for Irfan in helloworld.py

--- jenkins-101/helloworld.py
+++ jenkins-101/helloworld.py
@@ -6,3 +6,5 @@ print("Hello world kashif bhai aman")
 print("Hello world kashif bhai Hidaya")
 print("Hello world kashif bhai Hidaya")
 print("Hello world kashif bhai Hidaya")
+
+print("Hello world kashif bhai Irfan")
